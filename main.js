07c9f20dd1fc5615c155a042bf3d6343e2a0bfbf
madinahaidary <168135300+madinahaidary@users.noreply.github.com>
2024-05-25 12:19:00 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// 1. Declare variables and assign values
+let city = "Herat";
+let region = "Muslim";
+let population_density = 2.5; // people per square kilometer
+let area = 100; // square kilometers
+let language = "Persian";
+
+// 2. Log their values to the console
+console.log(`City: ${city}`);
+console.log(`Region: ${region}`);
+console.log(`Population Density: ${population_density} people/km²`);
+console.log(`Area: ${area} km²`);
+console.log(`Language:${language}`);
+
+// 3. Declare variables
+let isCapital = true; // Herat is not the capital of Afghanistan
+let officialLanguage = true; // no value assigned yet
+
+// 4. Log the types of variables to the console
+console.log(`Type of isCapital: ${typeof isCapital}`);
+console.log(`Type of population_density: ${typeof population_density}`);
+console.log(`Type of city: ${typeof city}`);
